513284c130e48a9ee17926aad72620379c5fb229
BrenoNevess <bbrenonevess@gmail.com>
2025-04-15 11:01:35 -0300

    Erro dos avisos corrigido

--- src/bootstrap-5.3.0-dist/bootstrap/js/alert.js
+++ src/bootstrap-5.3.0-dist/bootstrap/js/alert.js
@@ -1,18 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    function escondermsg(id, delay) {
-        const mensagem = document.getElementById(id);
-        if (mensagem) {
-            setTimeout(() => {
-                mensagem.style.transition = "opacity 0.5s ease";
-                mensagem.style.opacity = "0";
-                setTimeout(() => mensagem.remove(), 500);
-            }, delay);
-        }
+function escondermsg(id, delay) {
+    const mensagem = document.getElementById(id);
+    if (mensagem) {
+        setTimeout(() => {
+            mensagem.style.transition = "opacity 0.5s ease";
+            mensagem.style.opacity = "0";
+            setTimeout(() => mensagem.remove(), 500);
+        }, delay);
     }
+}
 
-    escondermsg('messagem-login', 5000);
-    escondermsg('message-error', 2000); 
-    escondermsg('message-success', 3000);   
+window.addEventListener('DOMContentLoaded', () => {
+    escondermsg('mensagem-login', 5000);
+    escondermsg('message-error', 2000);
+    escondermsg('message-success', 3000);
     escondermsg('message-update', 3000);
     escondermsg('message-delete', 3000);
 });
